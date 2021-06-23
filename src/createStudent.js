fc26293eecbea53bd36e6ed2582f13a9186d9fea
Carol_Inglethe <carolinainglethe@gmail.com>
2021-06-22 21:17:23 -0300

    Requesito 5 - Implementar funçao createStudent

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,15 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const pessoa = {
+    name: nome,
+    feedback: () => { 
+      const frase = 'Eita pessoa boa!'; 
+      return frase;
+    },
+  };
+  return pessoa;
+};
 
 module.exports = createStudent;
